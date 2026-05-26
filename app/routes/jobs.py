57edafe53d38d135d9b131d9bd807a70d364d779
Ansh Sharma <122905517+AnshSharma16@users.noreply.github.com>
2026-05-26 18:58:53 +0530

    Add FastAPI CRUD endpoints and request validation

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+
+'''from fastapi import APIRouter
 router=APIRouter()
 
 @router.get('/jobs')
@@ -12,4 +13,23 @@ def get_jobs():
          "company": "Anthropic",
             "role": "AI Automation Developer"
     }
-    ]
+    ]'''
+
+from fastapi import APIRouter
+from app.schemas.job_schema import Job
+
+router = APIRouter()
+
+jobs_db = []
+
+@router.get("/jobs")
+def get_jobs():
+    return jobs_db
+
+@router.post("/jobs")
+def create_job(job: Job):
+    jobs_db.append(job.dict())
+    return {
+        "message": "Job added successfully",
+        "job": job
+    }
